d09d7702a3662acda31c9dca295212d9065af6b5
Endy Muhardin <endy.muhardin@gmail.com>
2025-10-27 14:26:34 +0700

    unused imports

--- sample-apps/billing-provider/src/main/java/com/muhardin/endy/training/billing/entity/Billing.java
+++ sample-apps/billing-provider/src/main/java/com/muhardin/endy/training/billing/entity/Billing.java
@@ -4,9 +4,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import org.hibernate.annotations.Collate;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
