c99dcb199a4cd0718f790a91693d5b4f9119e86d
Endy Muhardin <endy.muhardin@gmail.com>
2025-10-27 14:44:52 +0700

    implement payment API

--- sample-apps/billing-provider/src/main/java/com/muhardin/endy/training/billing/controller/BillingController.java
+++ sample-apps/billing-provider/src/main/java/com/muhardin/endy/training/billing/controller/BillingController.java
@@ -1,14 +1,20 @@
 package com.muhardin.endy.training.billing.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.muhardin.endy.training.billing.entity.Billing;
+import com.muhardin.endy.training.billing.entity.Payment;
 import com.muhardin.endy.training.billing.repository.BillingRepository;
+import com.muhardin.endy.training.billing.repository.PaymentRepository;
 
 @RestController
 @RequestMapping("/api/billings")
@@ -16,6 +22,9 @@ public class BillingController {
 
     @Autowired
     private BillingRepository billingRepository;
+
+    @Autowired 
+    private PaymentRepository paymentRepository;
 
     @GetMapping("/")
     public List<Billing> findBillingByProductCodeAndCustomerNumber(String productCode, String customerNumber){
@@ -25,5 +34,21 @@ public class BillingController {
          System.out.println("Jumlah billing : "+billings.size());
 
          return billings;
+    }
+
+    @PostMapping("/{id}/payments")
+    public Payment payBilling(@PathVariable("id") Billing billing){
+        Payment p = new Payment();
+        p.setBilling(billing);
+        p.setAmount(billing.getAmount());
+        p.setPaymentReferences(UUID.randomUUID().toString());
+        p.setTransactionTime(LocalDateTime.now());
+
+        billing.setPaid(true);
+        billingRepository.save(billing);
+
+        paymentRepository.save(p);
+        System.out.println("Payment berhasil untuk billing id : "+billing.getId()+", payment reference : "+p.getPaymentReferences()  );
+        return p;
     }
 }
